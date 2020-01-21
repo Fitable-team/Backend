35eb27eedaeb369fa9119014f4d01f5a8b4df4a7
wheejuni <wheejuni@gmail.com>
2020-01-21 11:32:23 +0100

    스튜디오 회원 로그인아이디 변경 로직 추가

--- fittable-project/admin/src/main/java/net/fittable/admin/application/MemberManagementService.java
+++ fittable-project/admin/src/main/java/net/fittable/admin/application/MemberManagementService.java
@@ -3,12 +3,15 @@ package net.fittable.admin.application;
 import net.fittable.admin.infrastructure.components.generic.StudioOwnerIDGenerator;
 import net.fittable.admin.infrastructure.repositories.AdminMemberRepository;
 import net.fittable.admin.infrastructure.repositories.StudioOwnerMemberRepository;
+import net.fittable.domain.authentication.Member;
 import net.fittable.domain.authentication.StudioOwnerMember;
+import net.fittable.domain.authentication.enums.MemberAuthority;
 import net.fittable.domain.business.ContactInformation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.NoSuchElementException;
 
 @Service
 public class MemberManagementService {
@@ -32,5 +35,18 @@ public class MemberManagementService {
         String generatedPassword = this.idGenerator.setPassword(member);
 
         return studioOwnerMemberRepository.save(member);
+    }
+
+    @Transactional
+    public StudioOwnerMember changeStudioMemberLoginId(String loginId, Member member) {
+        if(member.getAuthority() != MemberAuthority.STUDIO_OWNER) {
+            throw new IllegalArgumentException("스튜디오 회원에게만 제공되는 기능입니다.");
+        }
+        StudioOwnerMember targetMember = studioOwnerMemberRepository.findByLoginId(member.getLoginId())
+                .orElseThrow(() -> new NoSuchElementException("해당 아이디로 검색된 스튜디오 회원이 없습니다."));
+
+        targetMember.setLoginId(loginId);
+
+        return studioOwnerMemberRepository.save(targetMember);
     }
 }
