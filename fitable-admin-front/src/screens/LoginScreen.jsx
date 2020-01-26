c61fa8f275cfbc73990179893bbf0ce92e022bad
wheejuni <wheejuni@gmail.com>
2020-01-26 11:35:40 +0100

    react-frontend intial commit

---
+++ fitable-admin-front/src/screens/LoginScreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class LoginScreen extends React.Component {
+    
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+            ''
+        )
+    }
+
+}
